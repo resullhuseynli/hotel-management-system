8fbd90b68d025f2d1950d1900eec7aa9c875aa09
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-29 20:42:18 +0400

    Validations added

--- src/main/java/com/booking/hotel/dto/HotelDTO.java
+++ src/main/java/com/booking/hotel/dto/HotelDTO.java
@@ -1,5 +1,7 @@
 package com.booking.hotel.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,6 +9,7 @@ import lombok.Setter;
 @Setter
 public class HotelDTO {
 
+    @Size(min = 1, max = 255) @NotNull
     private String name;
     private String location;
 
