6e2034259ecfbb8eb1488afc9d766409d5c26824
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-29 22:50:05 +0400

    MethodArgumentNotValidException handled

--- src/main/java/com/booking/hotel/exception/GlobalExceptionHandler.java
+++ src/main/java/com/booking/hotel/exception/GlobalExceptionHandler.java
@@ -1,14 +1,36 @@
 package com.booking.hotel.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler<T> {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ApiResponse> handleNotFoundException(NotFoundException notFoundException) {
-        return ResponseEntity.badRequest().body(new ApiResponse(notFoundException.getMessage()));
+    public ResponseEntity<ApiResponse<String>> handleNotFoundException(NotFoundException notFoundException) {
+        return ResponseEntity.badRequest().body(new ApiResponse<>(notFoundException.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Map<String, List<String>>>> handleValidationErrors(MethodArgumentNotValidException methodArgumentNotValidException) {
+        List<String> errors = methodArgumentNotValidException.getBindingResult().getFieldErrors()
+                .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+        return new ResponseEntity<>(new ApiResponse<>(getErrorsMap(errors)), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    private Map<String, List<String>> getErrorsMap(List<String> errors) {
+        Map<String, List<String>> errorResponse = new HashMap<>();
+        errorResponse.put("errors", errors);
+        return errorResponse;
     }
 }
