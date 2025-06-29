ecb2eef8901d6b2511935d58e454e795d51556fa
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-29 20:42:21 +0400

    Validations added

--- src/main/java/com/booking/hotel/controller/HotelController.java
+++ src/main/java/com/booking/hotel/controller/HotelController.java
@@ -3,21 +3,24 @@ package com.booking.hotel.controller;
 import com.booking.hotel.dto.HotelDTO;
 import com.booking.hotel.model.Hotel;
 import com.booking.hotel.service.HotelService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController("/api/v1/hotels")
 @RequiredArgsConstructor
+@Validated
 public class HotelController {
 
     private final HotelService hotelService;
 
     @PostMapping
-    public ResponseEntity<Hotel> addHotel(@RequestBody HotelDTO hotel) {
+    public ResponseEntity<Hotel> addHotel(@Valid @RequestBody HotelDTO hotel) {
         return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.addHotel(hotel));
     }
 
@@ -38,7 +41,7 @@ public class HotelController {
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Hotel> updateHotel(@RequestBody HotelDTO hotelDTO, @PathVariable Long id) {
+    public ResponseEntity<Hotel> updateHotel(@Valid @RequestBody HotelDTO hotelDTO, @PathVariable Long id) {
         return ResponseEntity.status(HttpStatus.OK).body(hotelService.updateHotel(hotelDTO, id));
     }
 }
