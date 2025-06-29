d28d0b95ef2652ee0b78bb28041f7174869350bb
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-06-29 20:41:39 +0400

    getAllRoomsByHotelId added

--- src/main/java/com/booking/hotel/dao/RoomDAO.java
+++ src/main/java/com/booking/hotel/dao/RoomDAO.java
@@ -3,5 +3,9 @@ package com.booking.hotel.dao;
 import com.booking.hotel.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RoomDAO extends JpaRepository<Room, Long> {
+    Optional<List<Room>> getAllRoomsByHotelId(long hotelId);
 }
