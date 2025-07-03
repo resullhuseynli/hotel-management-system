4050cd005708c1ac7c10aff595a2f8fb748384c0
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-03 19:53:35 +0400

    @Repository added

--- src/main/java/com/booking/hotel/dao/HotelDAO.java
+++ src/main/java/com/booking/hotel/dao/HotelDAO.java
@@ -2,6 +2,8 @@ package com.booking.hotel.dao;
 
 import com.booking.hotel.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface HotelDAO extends JpaRepository<Hotel, Long> {
 }
