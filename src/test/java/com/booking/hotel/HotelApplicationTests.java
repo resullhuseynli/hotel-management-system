be067060d3b1b002f611cf54142bf981d0cffaa8
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-06 20:03:02 +0400

    deleted all implementations and annotations about tests

--- src/test/java/com/booking/hotel/HotelApplicationTests.java
+++ src/test/java/com/booking/hotel/HotelApplicationTests.java
@@ -1,8 +1,5 @@
 package com.booking.hotel;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 class HotelApplicationTests {
 
     void contextLoads() {
