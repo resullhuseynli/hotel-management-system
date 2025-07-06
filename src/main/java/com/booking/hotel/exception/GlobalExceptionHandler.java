ac7eb336b2789b2aa3cecd6178cd0c0d1ea6f20c
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-06 20:48:26 +0400

    logging added

--- src/main/java/com/booking/hotel/exception/GlobalExceptionHandler.java
+++ src/main/java/com/booking/hotel/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.booking.hotel.exception;
 
+import com.booking.hotel.statics.ErrorCodes;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +15,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler<T> {
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ApiResponse<String>> handleNotFoundException(NotFoundException notFoundException) {
+        log.error(ErrorCodes.NOT_FOUND_ERROR_CODE, notFoundException.getMessage());
         return ResponseEntity.badRequest().body(new ApiResponse<>(notFoundException.getMessage()));
     }
 
@@ -25,11 +29,13 @@ public class GlobalExceptionHandler<T> {
     public ResponseEntity<ApiResponse<Map<String, List<String>>>> handleValidationErrors(MethodArgumentNotValidException methodArgumentNotValidException) {
         List<String> errors = methodArgumentNotValidException.getBindingResult().getFieldErrors()
                 .stream().map(FieldError::getDefaultMessage).collect(Collectors.toList());
+        log.error(ErrorCodes.VALIDATION_ERROR_CODE, errors);
         return new ResponseEntity<>(new ApiResponse<>(getErrorsMap(errors)), new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RoomIsAlreadyBookedException.class)
     public ResponseEntity<ApiResponse<String>> handleRoomIsAlreadyBookedException(RoomIsAlreadyBookedException roomIsAlreadyBookedException) {
+        log.error(ErrorCodes.ROOM_IS_ALREADY_BOOKED_ERROR_CODE, roomIsAlreadyBookedException.getMessage());
         return ResponseEntity.badRequest().body(new ApiResponse<>(roomIsAlreadyBookedException.getMessage()));
     }
 
---
+++ src/main/java/com/booking/hotel/statics/ErrorCodes.java
@@ -0,0 +1,9 @@
+package com.booking.hotel.statics;
+
+public class ErrorCodes {
+
+    public static final String VALIDATION_ERROR_CODE = "Validation error handled: {}";
+    public static final String NOT_FOUND_ERROR_CODE = "Not Found error handled: {}";
+    public static final String ROOM_IS_ALREADY_BOOKED_ERROR_CODE = "Room is already booked error handled: {}";
+
+}
