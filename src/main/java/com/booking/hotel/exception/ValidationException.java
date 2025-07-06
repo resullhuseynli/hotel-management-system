ea007f940cf493fc7e5734d5cb5f9bea329b95f5
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-06 20:48:16 +0400

    unused exception deleted

--- src/main/java/com/booking/hotel/exception/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.booking.hotel.exception;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
