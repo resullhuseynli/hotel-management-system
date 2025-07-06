903a47adc28c389c4cd263c75e6bb600ddd8320e
Resul Huseynli <117728546+resullhuseynli@users.noreply.github.com>
2025-07-06 20:00:33 +0400

    Builder pattern added and column structures changed

--- src/main/java/com/booking/hotel/dao/model/Hotel.java
+++ src/main/java/com/booking/hotel/dao/model/Hotel.java
@@ -1,27 +1,30 @@
-package com.booking.hotel.model;
+package com.booking.hotel.dao.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
-@RequiredArgsConstructor
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Hotel {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Size(min = 1, max = 255) @NotNull
+    @Column(nullable = false)
     private String name;
     private String location;
-    private final LocalDateTime createdAt = LocalDateTime.now();
+    @CreationTimestamp
+    @Column(nullable = false,  updatable = false)
+    private LocalDateTime createdAt;
 
     @OneToMany(mappedBy = "hotel",cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Room> rooms;
